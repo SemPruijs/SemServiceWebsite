d9d1a04a85c518ca3cd866ad4901650564183b32
SemPruijs <sem@pruijs.nl>
2020-05-17 22:47:44 +0200

    Add better styling to the page

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 function loadItems() {
     let request = new XMLHttpRequest();
-    request.open('GET', 'https://htun05wn1f.execute-api.us-east-1.amazonaws.com/prod/stock', true)
+    request.open('GET', 'https://htun05wn1f.execute-api.us-east-1.amazonaws.com/prod/stock', true);
     request.onload = function() {
         let data = JSON.parse(this.response)
         // let data = {items: [{"name": "White Oreo", "price": 0.45}, {"name": "White KitKat", "price": 0.99}]};
@@ -15,7 +15,7 @@ function loadItems() {
 
             let a = document.createElement("A");
             li.appendChild(a);
-            a.setAttribute("href", "https://bunq.me/SemService/" + item.price + "/" + item.name)
+            a.setAttribute("href", "https://bunq.me/SemService/" + item.price + "/" + item.name);
 
             let text = document.createTextNode(item.name);
             a.appendChild(text);
